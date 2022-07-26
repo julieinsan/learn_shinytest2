256f5e4972e8dc5cb36ea18db74dde00e267a34c
julieinsan <julieinsignares@gmail.com>
2022-07-26 20:28:27 +0000

    testing action

--- git_setup.R
+++ git_setup.R
@@ -1,7 +1,7 @@
 library(usethis)
 library(gitcreds)
 
-
+# This is a message
 usethis::use_git_config(user.name = "julieinsan", user.email = "julieinsignares@gmail.com")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
