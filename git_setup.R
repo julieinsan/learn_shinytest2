d138c15f67107feca686101374577fbee290b613
julieinsan <julieinsignares@gmail.com>
2022-07-26 19:00:17 +0000

    I made some changes

--- git_setup.R
+++ git_setup.R
@@ -2,9 +2,11 @@ library(usethis)
 library(gitcreds)
 
 
-usethis::use_git_config(user.name = "user name", user.email = "user@email.com")
+usethis::use_git_config(user.name = "julieinsan", user.email = "julieinsignares@gmail.com")
 usethis::create_github_token()
 gitcreds::gitcreds_set()
 
 usethis::use_git()
+
 usethis::use_github()
+3
